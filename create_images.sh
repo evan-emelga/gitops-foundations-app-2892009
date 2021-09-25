780534251786bf926b4b5b4a00770f2dc4673f4f
Evan Emelga <evan.emelga@go-jek.com>
2021-09-25 20:20:57 +0700

    Create service A and B

--- create_images.sh
+++ create_images.sh
@@ -1,5 +1,9 @@
-docker build -t $1/gitops-foundations:1.0 --build-arg JAR_FILE=gitops-foundations-1.0.0.RELEASE.jar .
-docker build -t $1/gitops-foundations:2.0 --build-arg JAR_FILE=gitops-foundations-2.0.0.RELEASE.jar .
+docker build -t $1/gitops-foundations-service-a:1.0 --build-arg JAR_FILE=gitops-foundations-service-a-1.0.0.RELEASE.jar .
+docker build -t $1/gitops-foundations-service-a:2.0 --build-arg JAR_FILE=gitops-foundations-service-a-2.0.0.RELEASE.jar .
+docker build -t $1/gitops-foundations-service-b:1.0 --build-arg JAR_FILE=gitops-foundations-service-b-1.0.0.RELEASE.jar .
+docker build -t $1/gitops-foundations-service-b:2.0 --build-arg JAR_FILE=gitops-foundations-service-b-2.0.0.RELEASE.jar .
 
-docker push $1/gitops-foundations:1.0
-docker push $1/gitops-foundations:2.0
+docker push $1/gitops-foundations-service-a:1.0
+docker push $1/gitops-foundations-service-a:2.0
+docker push $1/gitops-foundations-service-b:1.0
+docker push $1/gitops-foundations-service-b:2.0
